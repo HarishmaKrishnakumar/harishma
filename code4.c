91d3d00e60a54a72e2e20a87349866b09e9964d7
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-01-20 19:36:38 +0530

    Create code4.c

---
+++ code4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+ char c;
+ scanf("%c",&c);
+ if((c>=65 && c<=90) ||	 (c>=97 && c<=122))
+{
+    printf("Alphabet");
+}
+
+else
+{
+    printf("not");
+}
+    return 0;
+}

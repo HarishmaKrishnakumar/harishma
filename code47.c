9856ed9d8f18a0259c7e93199b11a2c08f88d21a
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-01-26 19:58:45 +0530

    code47.c

---
+++ code47.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+   int N;
+   scanf("%d",&N);
+   
+   if(N>=1 && N<=10)
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+ 
+  return 0;
+}

071adad26c043bb5f07c42964d31503df2aea419
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-02-17 20:06:06 +0530

    Create codep11.c

---
+++ codep11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h> 
+
+int main()
+{
+    char s[20];
+    scanf("%s",s);
+  if(s[0]=='s' || s[0]=='S')
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+ 
+    return 0;
+}

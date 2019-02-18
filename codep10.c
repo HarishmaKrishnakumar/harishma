821751e696c3134a337431125bba72c058df326b
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-02-18 14:47:02 +0530

    Create codep10.c

---
+++ codep10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+  char str[10000],tot[10000];
+  int i,k=0;
+  gets(str);
+  gets(tor);
+  for(i=0;str[i]!='\0';i++)
+  {  
+          if(str[i]!=tot[i])
+          {
+              
+             k++; 
+           
+
+          }
+   
+      }
+  
+  if(k==1)
+  printf("yes");
+  else
+  printf("no");
+    return 0;
+}

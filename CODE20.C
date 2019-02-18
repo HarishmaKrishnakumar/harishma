9de606424b764f5b458e312d4fa9f7f4827d4013
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-02-18 14:10:13 +0530

    Update CODE20.C

--- CODE20.C
+++ CODE20.C
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
- char string[50],str1;
- int n,i;
+ char string[50],string1;
+ int N,i;
  scanf("%s",string);
  for(i=0;string[i]!='\0';i++)
  {
-  n=string[i];
-  if(n>=88 && n<=90 || n>=120 && n<=122)
+  N=string[i];
+  if(N>=88 && N<=90 || N>=120 && N<=122)
   {
-   n=n-23;
-   str1=n;
+   N=N-23;
+   string1=N;
   }
   else
   {
-  str1=n+3;
+  string1=N+3;
   }
-  string[i]=str1;
+  string[i]=string1;
  }
  printf("%s",string);
  return 0;
